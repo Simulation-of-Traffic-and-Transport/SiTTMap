ad840137467b24966fe715c9d73a2632d337bc72
Maximilian Kalus <info@auxnet.de>
2022-12-16 10:48:07 +0100

    Render basic map.

---
+++ src/components/SittMap.vue
@@ -0,0 +1,23 @@
+<template>
+	<div style="height: 600px; width: 800px">
+		<LMap ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+			<LTileLayer
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				layer-type="base"
+				name="OpenStreetMap"
+			></LTileLayer>
+		</LMap>
+	</div>
+</template>
+
+<script setup>
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { ref } from "vue";
+
+// data
+const zoom = ref(2);
+</script>
+
+<style>
+@import "leaflet/dist/leaflet.css";
+</style>
