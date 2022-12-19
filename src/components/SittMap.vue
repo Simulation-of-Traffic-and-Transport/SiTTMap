174e57bcfdc1bc8284eb21be439f4df1bda447d8
Maximilian Kalus <info@auxnet.de>
2022-12-19 10:12:40 +0100

    GeoJSON to polyline to ass stroke colors more easily.

--- src/components/SittMap.vue
+++ src/components/SittMap.vue
@@ -18,14 +18,27 @@
 				name="OpenStreetMap"
 				attribution="&copy; <a target='_blank' href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
 			></LTileLayer>
+
 			<LLayerGroup>
-				<LGeoJson v-for="path in data.paths" :key="path.id" :geojson="path.geom">
+				<LPolyline
+					v-for="path in paths"
+					:key="path.id"
+					:lat-lngs="path.latLngs"
+					:name="path.id"
+					:title="path.id"
+					:weight="hoverId === path.id ? 5 : 3"
+					:color="hoverId === path.id ? '#8b0000' : '#3388ff'"
+					@mouseover="onHubMouseOver(path.id)"
+					@mouseout="onHubMouseOut(path.id)"
+				>
 					<LPopup>
 						<div class="font-bold mb-2">{{ path.id }}</div>
-						<div>TODO</div>
+						<div>{{ path.from }} &ndash; {{ path.to }}</div>
+						<div>Length: {{ Math.round(path.length_m) }}m</div>
 					</LPopup>
-				</LGeoJson>
+				</LPolyline>
 			</LLayerGroup>
+
 			<LLayerGroup>
 				<LCircleMarker
 					v-for="hub in hubs"
@@ -35,9 +48,10 @@
 					:name="hub.id"
 					:title="hub.id"
 					:fill="true"
-					:fill-color="hoverId == hub.id ? '#f00' : '#00f'"
-					@mouseover="onHubMouseOver(hub)"
-					@mouseout="onHubMouseOut(hub)"
+					:fill-color="hoverId === hub.id ? '#8b0000' : '#3388ff'"
+					:fill-opacity="hoverId === hub.id ? 0.7 : 0.2"
+					@mouseover="onHubMouseOver(hub.id)"
+					@mouseout="onHubMouseOut(hub.id)"
 				>
 					<LPopup>
 						<div class="font-bold mb-2">{{ hub.id }}</div>
@@ -56,7 +70,7 @@
  * Show the map
  */
 import L from "leaflet";
-import { LCircleMarker, LGeoJson, LLayerGroup, LMap, LPopup, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { LCircleMarker, LLayerGroup, LMap, LPolyline, LPopup, LTileLayer } from "@vue-leaflet/vue-leaflet";
 import { computed, ref } from "vue";
 
 // props
@@ -73,11 +87,21 @@ const map = ref();
 const hoverId = ref(null);
 
 // computed data
+const paths = computed(() =>
+	props.data.paths.map((path) => ({
+		id: path.id,
+		from: path.from,
+		to: path.to,
+		length_m: path.length_m,
+		latLngs: path.geom.coordinates.map((coord) => [coord[1], coord[0]]), // leaflet lat/lng switch
+		heights: path.geom.coordinates.map((coord) => coord[2]),
+	}))
+);
 const hubs = computed(() =>
 	props.data.nodes.map((node) => ({
 		id: node.id,
 		overnight: node.overnight,
-		latLng: [node.geom.coordinates[1], node.geom.coordinates[0]],
+		latLng: [node.geom.coordinates[1], node.geom.coordinates[0]], // leaflet lat/lng switch
 		height: node.geom.coordinates[2],
 	}))
 );
@@ -87,24 +111,22 @@ const onMapReady = (readyMap) => {
 	// map to ref variable
 	map.value = readyMap;
 
-	// TODO
-	console.log(props.data);
-
 	const bounds = L.latLngBounds();
 
 	for (const node of props.data.nodes) {
 		bounds.extend(L.latLng([node.geom.coordinates[1], node.geom.coordinates[0]]));
 	}
+	// TODO: might make sense to add the data of the paths, too
 
 	readyMap.fitBounds(bounds, { padding: [10, 10] });
 };
 
-const onHubMouseOver = (hub) => {
-	hoverId.value = hub.id;
+const onHubMouseOver = (id) => {
+	hoverId.value = id;
 };
 
 // eslint-disable-next-line no-unused-vars
-const onHubMouseOut = (hub) => {
+const onHubMouseOut = (id) => {
 	hoverId.value = "";
 };
 </script>
