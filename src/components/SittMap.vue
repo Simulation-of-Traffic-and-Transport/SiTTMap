2c70db124a8b80e22bc80765b84dfb7d5e1e51a6
Maximilian Kalus <info@auxnet.de>
2025-10-16 08:04:18 +0200

    Remove old debug logging.

--- src/components/SittMap.vue
+++ src/components/SittMap.vue
@@ -455,8 +455,6 @@ const hasCancelledAgent = (uids) => {
 let oldTimeStamp;
 
 const sliderPlayingLoop = (timeStamp) => {
-	console.log(timeStamp);
-
 	// Calculate the number of seconds passed since the last frame
 	if (!oldTimeStamp) {
 		slider.value += 0.01;
